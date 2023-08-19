a412e0da11d9e362cb023f7cb80d3f672d68976f
Salocin0 <nicolasarieldurelli@gmail.com>
2023-08-19 14:21:11 -0300

    fix eerror

--- src/services/errors/custom-error.js
+++ src/services/errors/custom-error.js
@@ -1,11 +1,10 @@
 export default class CustomError {
-    static createError({ name = 'error', cause, message, code }) {
-      const error = new Error(message);
-      error.name = name;
-      error.cause = cause;
-      error.code = code;
-  
-      throw error;
-    }
+  static createError({ name = 'error', cause, message, code }) {
+    const error = new Error(message);
+    error.name = name;
+    error.code = code;
+    error.cause = cause;  // Attach the cause property directly
+
+    throw error;
   }
-  
+}
